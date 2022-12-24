5ededcefe3506ddc504a3f0519716815a461f196
mgaray99 <junior.garay1999@gmail.com>
2022-12-23 21:33:08 -0600

    Made Wolves only show either the angry texture or the wild texture, no longer showing the tame texture

--- src/main/java/com/infamous/call_of_the_wild/client/ModClientEventHandler.java
+++ src/main/java/com/infamous/call_of_the_wild/client/ModClientEventHandler.java
@@ -4,6 +4,7 @@ import com.infamous.call_of_the_wild.CallOfTheWild;
 import com.infamous.call_of_the_wild.common.registry.COTWEntityTypes;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.entity.WolfRenderer;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Wolf;
 import net.minecraftforge.api.distmarker.Dist;
@@ -13,21 +14,28 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = CallOfTheWild.MODID, value = Dist.CLIENT)
 public class ModClientEventHandler {
+    private static final ResourceLocation WOLF_LOCATION = new ResourceLocation("textures/entity/wolf/wolf.png");
+    private static final ResourceLocation WOLF_ANGRY_LOCATION = new ResourceLocation("textures/entity/wolf/wolf_angry.png");
 
     @SubscribeEvent
     static void onRegisterLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event){
         event.registerLayerDefinition(COTWModelLayers.DOG, AndreDogModel::createBodyLayer);
     }
 
+    @SuppressWarnings("NullableProblems")
     @SubscribeEvent
     static void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event){
         event.registerEntityRenderer(COTWEntityTypes.DOG.get(), DogRenderer::new);
         event.registerEntityRenderer(EntityType.WOLF, context -> new WolfRenderer(context){
-            @SuppressWarnings("NullableProblems")
             @Override
             protected void scale(Wolf wolf, PoseStack poseStack, float partialTick) {
                 float scaleFactor = 1.25F;
                 poseStack.scale(scaleFactor, scaleFactor, scaleFactor);
+            }
+
+            @Override
+            public ResourceLocation getTextureLocation(Wolf wolf) {
+                return wolf.isAngry() ? WOLF_ANGRY_LOCATION : WOLF_LOCATION;
             }
         });
     }
